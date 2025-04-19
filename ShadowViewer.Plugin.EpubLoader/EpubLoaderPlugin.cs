061a127f24fd8a25a1485256c47143e31e9abb7e
kitUIN <kulujun@gmail.com>
2025-04-19 13:10:26 +0800

    Init

---
+++ ShadowViewer.Plugin.EpubLoader/EpubLoaderPlugin.cs
@@ -0,0 +1,41 @@
+﻿using ShadowPluginLoader.Attributes;
+using ShadowViewer.Core.Plugins;
+using ShadowViewer.Plugin.EpubLoader.I18n;
+
+
+namespace ShadowViewer.Plugin.EpubLoader;
+
+[MainPlugin]
+[CheckAutowired]
+public partial class EpubLoaderPlugin : AShadowViewerPlugin
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    partial void ConstructorInit()
+    {
+    }
+    /// <inheritdoc/>
+    public override string DisplayName => I18N.DisplayName;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void Enabled()
+    {
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public override void Loaded()
+    {
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void Disabled()
+    {
+    }
+}
